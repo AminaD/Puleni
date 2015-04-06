d627bb551c23e798e8f375cb178af205cd378fe1
neidm <nb15321@etf.unsa.ba>
2015-04-06 13:20:21 +0000

    commit prije checkouta

--- .c9/metadata/workspace/app/assets/javascripts/angular.js
+++ .c9/metadata/workspace/app/assets/javascripts/angular.js
@@ -1,1 +1,1 @@
-{"filter":false,"title":"angular.js","tooltip":"/app/assets/javascripts/angular.js","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":0,"scrollleft":0,"selection":{"start":{"row":7,"column":0},"end":{"row":7,"column":0},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":0},"timestamp":1389816244000,"hash":"8883e29690d7193e8bd858516ffb309d0aff25cb"}
+{}
